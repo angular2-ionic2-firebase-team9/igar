dd91491118cf9dc9e51f49dfef7054afdb449f90
Allie Kim <yealee.kim87@gmail.com>
2017-02-28 14:19:38 +0900

    category 페이지 리스트 추가

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { CategoryProvider } from '../../providers/category-provider';
+import { Category } from '../../models/category';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  categories : Category[];
 
+  constructor(public navCtrl: NavController, public categoryProvider : CategoryProvider) {
+
+
+    categoryProvider.load().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  itemSelected(category: Category) {
+    console.log(category)
   }
 
 }
