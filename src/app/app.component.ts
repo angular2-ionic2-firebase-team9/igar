11a22a13fdcea55b90c3f2d019364882f151d890
youngbinlee <youngbin.lee@gmail.com>
2017-03-14 23:57:06 +0900

    auto login  추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,30 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
-
+import { AuthService } from '../providers/auth-service';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = LoginPage;
 
-  constructor(platform: Platform) {
+  rootPage : any;
+
+  constructor(platform: Platform, public authService: AuthService) {
+
+
+    authService.isLoggedIn.subscribe(isLoggedIn => {
+      if(isLoggedIn){
+        console.log('authenticated')
+        this.rootPage = TabsPage;
+      }else{
+        console.log('authenticated fail')
+        this.rootPage = LoginPage;
+      }
+
+    });
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
